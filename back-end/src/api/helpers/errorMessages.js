b8fc109a4a22af1a54f9c15dfa9ee0b09250cb9c
rphsantos1995 <rph.santos@Outlook.com>
2022-06-03 19:54:06 -0300

    apiPath: moving controller and userservice into api folder

---
+++ back-end/src/api/helpers/errorMessages.js
@@ -0,0 +1,5 @@
+module.exports = {
+    INTERNAL: 'Internal error',
+    NF: 'Not found',
+    CONF: 'User already created',
+};
--- back-end/src/database/helpers/errorMessages.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    INTERNAL:'Internal error',
-    NF:'Not found'
-}
