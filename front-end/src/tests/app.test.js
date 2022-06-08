9a95a9130d53bdcee66337bbe3d7a368e6fa3c7b
rphsantos1995 <rph.santos@Outlook.com>
2022-06-08 16:58:28 -0300

    Test: remove temp button test from source

--- front-end/src/tests/app.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Buttons from '../components/Buttons';
-
-test('O header possui todos os links de navegação', () => {
-  render(<Buttons />);
-  const button = screen.getByRole('button');
-  expect(button.type).toBe('button')
-  });
