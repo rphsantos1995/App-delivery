4d76eeecef11bfe572b7fdd6da99cd2775d48291
arturbacilla <artur.bacilla1@gmail.com>
2022-06-09 17:24:38 -0300

    :sparkles: feat: merged register

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
-// import CardProduct from '../components/CardProduct';
+import React, { useEffect, useState } from 'react';
+import CardProduct from '../components/CardProduct';
 import Navbar from '../components/Navbar';
 import { requestGet } from '../services/api';
 
 export default function Products() {
-  // const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -17,18 +17,17 @@ export default function Products() {
     <div>
       <Navbar userRole="customer" />
       <p>Tela de produtos</p>
-      {/* {
-        products.map((product, index) => (
+      {
+        products.map((product) => (
           <CardProduct
             key={ product.id }
             id={ product.id }
             price={ product.price }
             image={ product.url_image }
-            index={ index + 1 }
             name={ product.name }
           />
         ))
-      } */}
+      }
     </div>
   );
 }
