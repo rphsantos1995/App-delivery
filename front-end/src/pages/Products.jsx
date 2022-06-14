382a18f58961e1d47f4668b92fb347271dae21d1
Anderson-Alencar <anderson23alencar@gmail.com>
2022-06-14 17:23:45 -0300

    :sparkles: price total after add itens to cart

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CardProduct from '../components/CardProduct';
 import Navbar from '../components/Navbar';
+import ProductsContext from '../context/ProductsContext';
+import dataTestIds from '../helpers/dataTestIds';
 import { requestGet } from '../services/api';
 
 export default function Products() {
+  const { cart, allTotalPrice, setAllTotalPrice } = useContext(ProductsContext);
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -13,10 +16,26 @@ export default function Products() {
     })();
   }, []);
 
+  useEffect(() => {
+    if (cart.length > 0) {
+      const sum = cart
+        .map(({ quantity, price }) => quantity * price)
+        .reduce((acc, cur) => acc + cur)
+        .toFixed(2)
+        .replace('.', ',');
+      setAllTotalPrice(sum);
+    } else {
+      setAllTotalPrice(0);
+    }
+  }, [cart, setAllTotalPrice]);
+
   return (
     <div>
       <Navbar userRole="customer" />
       <p>Tela de produtos</p>
+      <span data-testid={ dataTestIds[21] }>
+        { allTotalPrice}
+      </span>
       {
         products.map((product) => (
           <CardProduct
