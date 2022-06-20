69ab6b5c62fc5487585ec8d997b3f1f2a90139d0
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-06-20 18:51:45 -0300

    :sparkles: Implementação do endpoit Get a aplicação

--- back-end/src/api/router/SalesRouter.js
+++ back-end/src/api/router/SalesRouter.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { create } = require('../controllers/SalesController');
+const { create, read } = require('../controllers/SalesController');
 
 const router = Router();
 const userRouter = router;
 
+router.get('/', read);
 router.post('/', create);
 
 module.exports = userRouter;
