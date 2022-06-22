f6089dabd7270d008fc5f64a98e9edb89bb49e1e
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-06-22 12:42:47 -0300

    :sparkles: Implementação da rota sales/:id a aplicação

--- back-end/src/api/router/SalesRouter.js
+++ back-end/src/api/router/SalesRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { create, read } = require('../controllers/SalesController');
+const { create, read, readOne } = require('../controllers/SalesController');
 
 const router = Router();
 const salesRouter = router;
 
 router.get('/', read);
+router.get('/:id', readOne);
 router.post('/', create);
 
 module.exports = salesRouter;
