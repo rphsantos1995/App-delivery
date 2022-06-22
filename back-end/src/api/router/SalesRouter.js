7e604f451b42520796c84fcd6bd0da1d7ce2efe3
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-06-22 13:07:06 -0300

    :sparkles: Rota /sales
    Criação da rota PUT /sales/:id para a api.

--- back-end/src/api/router/SalesRouter.js
+++ back-end/src/api/router/SalesRouter.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
-const { create, read, readOne } = require('../controllers/SalesController');
+const { create, read, readOne, updateStatus } = require('../controllers/SalesController');
 
 const router = Router();
 const salesRouter = router;
 
 router.get('/', read);
 router.get('/:id', readOne);
+router.put('/:id', updateStatus);
 router.post('/', create);
 
 module.exports = salesRouter;
