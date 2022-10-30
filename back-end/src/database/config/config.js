f5b1aa99bc541397af37aaccd24ded9533f8f1be
rphsantos1995 <rph.santos@Outlook.com>
2022-10-30 00:37:46 -0300

    connect to db container

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -12,9 +12,9 @@ const suffix = {
 
 const options = {
   host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
+  port: process.env.MYSQL_PORT || '3366',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+    `${process.env.MYSQL_DB_NAME || 'deliveryApp'}${suffix[environment] || suffix.test}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
